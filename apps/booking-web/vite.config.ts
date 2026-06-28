8eee40831ee2ee5ad64797ef25e5872b984e67ff
Kyle Khus <kkhus5@berkeley.edu>
2026-06-28 15:35:37 -0700

    [feat] install tailwind, axios, react query

--- apps/booking-web/vite.config.ts
+++ apps/booking-web/vite.config.ts
@@ -1,6 +1,14 @@
+import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-    plugins: [react()],
+    plugins: [react(), tailwindcss()],
+    server: {
+        proxy: {
+            "/api": {
+                target: "http://localhost:3001",
+            },
+        },
+    },
 });
