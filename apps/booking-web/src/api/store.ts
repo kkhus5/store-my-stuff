fd44ed738f5014069ab47fc4a2fcb8bb3438e53f
Kyle Khus <kkhus5@berkeley.edu>
2026-06-28 17:09:43 -0700

    [feat] flesh out store details section on booking/reservation page

--- apps/booking-web/src/api/store.ts
+++ apps/booking-web/src/api/store.ts
@@ -5,9 +5,20 @@ type GetAllStoresResponse = {
     stores: Store[];
 };
 
+type GetStoreResponse = {
+    store: Store;
+};
+
 export const store = {
     getAllStores: async (): Promise<GetAllStoresResponse> => {
         const { data } = await axiosClient.get<GetAllStoresResponse>("/store");
+        return data;
+    },
+
+    getStore: async (storeId: string): Promise<GetStoreResponse> => {
+        const { data } = await axiosClient.get<GetStoreResponse>(
+            `/store/${storeId}`,
+        );
         return data;
     },
 };
